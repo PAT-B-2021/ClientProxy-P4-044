02032a7f5e44a66538dcdcb94af86da9c0f38835
Aldi-nafi43 <aldi.nafi43@gmail.com>
2021-11-22 22:52:52 +0700

    commit

---
+++ ClientProxy-P4-044/ClientProxy_P4_044/ServiceReference1/MathFault.cs
@@ -0,0 +1,8 @@
+﻿namespace ClientProxy_P4_044.ServiceReference1
+{
+    internal class MathFault
+    {
+        public bool Kode { get; internal set; }
+        public bool Pesan { get; internal set; }
+    }
+}
